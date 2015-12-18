89380075f196cd996b48d645cbc43079c83925ab
zach powers <zcharlop@rockefeller.edu>
2015-12-18 11:57:34 -0500

    have components rendering via lifecycle methods but still not qute getting the whole props thing. I switched from a vector to a map but ran into trouble so I am once agian explicity passing the state atom into the components. However this is not updating on the component willl updata portion

--- src/d3map_reagent/core.cljs
+++ src/d3map_reagent/core.cljs
@@ -19,63 +19,144 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
+;;;---- Basic DB ----------------
+(def dbstart {:countdata {:data [{:samplename "DFD_1128.1",
+                                    :Longitude  -73.9185,
+                                    :Latitude   40.5879,
+                                    :totalreads 50095,
+                                    :countdata  {"A40926"       0,
+                                                 "A47934"       63,
+                                                 "A54145"       0,
+                                                 "actinomycin"  0,
+                                                 "aculeximycin" 0}
+                                    :size       5}
+                                   {:samplename "DFD_1128.1",
+                                    :Longitude  -73.8185,
+                                    :Latitude   40.7879,
+                                    :totalreads 50095,
+                                    :countdata  {"A40926"       0,
+                                                 "A47934"       63,
+                                                 "A54145"       0,
+                                                 "actinomycin"  0,
+                                                 "aculeximycin" 0}
+                                    :size       5}]}
+
+                         :molecules ["A40926"
+                                     "A47934"
+                                     "A54145"
+                                     "actinomycin"
+                                     "aculeximycin"]
+
+                         :activemolecule nil
+                         :normalized true
+              })
+
+;;;---- Helper Functions-----------
+(defn calculate-countdata [sample molecule]
+  "calculate the counts of given molecule"
+  (let [molnum (get-in sample [:countdata molecule])
+        sampletotal (:totalreads sample)]
+    (assoc sample :size (/ molnum sampletotal))))
+
+
 ;;;---- Event handlers-----------
 (register-handler                 ;; setup initial state
-  :initialize-mapdata                     ;; usage:  (submit [:initialize])
+  :initialize-db                     ;; usage:  (submit [:initialize])
   (fn
     [_ _]
-    (GET "js/nyc.json" {:response-format :json
-                       :handler         #(re-frame/dispatch [:process-mapdata %1])})
-    _))
+   dbstart))
 
 (register-handler                 ;; setup initial state
   :initialize-countdata                     ;; usage:  (submit [:initialize])
   (fn
-    [_ _]
+    [db _]
     (GET "js/counts.json" {:response-format :json
-                        :handler         #(re-frame/dispatch [:process-mapdata %1])})
-    _))
-
-(register-handler                 ;; setup initial state
-  :process-mapdata                ;; usage:  (submit [:initialize])
-  (fn
-    [db [_ resp]]
-    (merge db :mapdata #js [resp])))    ;; what it returns becomes the new state
+                           :handler         #(re-frame/dispatch [:process-countdata %1])})
+    db))
 
 (register-handler                 ;; setup initial state
   :process-countdata                ;; usage:  (submit [:initialize])
   (fn
     [db [_ resp]]
-    (let [counts (js->clj resp)]
-    (merge db :counts counts))))    ;; what it returns becomes the new state
+    (let [counts (js->clj resp :keywordize-keys true)]
+    (assoc db :countdata {:data counts}))))    ;; what it returns becomes the new state
+
+(register-handler
+  :update-active
+  (fn
+    [db [_ molvalue]]
+    (let [countdata (-> db :countdata :data)
+          newvalues (map #(calculate-countdata % molvalue) countdata)]
+      (println countdata)
+      (println newvalues)
+    (-> db (assoc :activemolecule molvalue)
+           (assoc :countdata {:data newvalues})))))  ;; what it returns becomes the new state
 
 
 ;;;----Subscription Handlers -----------
 (register-sub
-  :mapdata
-  (fn
-    [db _]                       ;; db is the app-db atom
-    (reaction (:mapdata @db))))    ;; wrap the computation in a reaction
-
-(register-sub
   :countdata
   (fn
-    [db _]                       ;; db is the app-db atom
-    (reaction (:countdata @db))))    ;; wrap the computation in a reaction
+    [db _]
+    (reaction (-> @db :countdata :data))))
+
 
 (register-sub
-  :top-10
+  :molecules
+  (fn
+    [db _]
+    (let [countdata (reaction (-> @db :countdata :data))
+          molecules (reaction (-> @countdata first :countdata keys))]
+      molecules)))
+
+(register-sub
+  :activemolecule
   (fn
     [db _]                       ;; db is the app-db atom
-    (reaction (:mapdata @db))))    ;; wrap the computation in a reaction
-
+    (reaction (:activemolecule @db))))    ;; wrap the computation in a reaction
 
 ;;;---------------
 
 
+(defn load-map [file svg path]
+  "use d3.json to load the json file and crete the map in the svg element usin the path"
+  (.json js/d3 file
+    (fn [error json]
+      (.. svg (append "g")
+          (attr "id" "boroughs")
+          (selectAll ".state")
+          (data (.-features json))
+          enter
+          (append "path")
+          (attr "class" (fn [d] (str "borough " (.. d -properties -borough))))
+          (attr "d" path)
+          (style "fill" (fn [d]  (.-colors d)))))))
+
+(defn draw-points [svg counts]
+  "use d3.json to load the json file and crete the map in the svg element usin the path"
+  (.. svg
+      (append "g")
+      (attr "id" "circle")
+      (selectAll "circle")
+      (data counts)
+      enter
+      (append "svg:circle")
+      (attr "cx" (fn [d] (.-d3lat d)))
+      (attr "cy" (fn [d] (.-d3lon d)))
+      (attr "r"  (fn [d] (.-size d)))
+      (attr "class" "bubble")
+      ))
 
 
-(defn NYCmap-inner []
+(defn molecule-select-box []
+  ""
+  (let [molecules (re-frame/subscribe [:molecules])]
+    (fn []
+      [:select {:on-change #(re-frame/dispatch [:update-active (-> % .-target .-value)])}
+       (for [m @molecules]
+         [:option {:value  (str m)} (str m)])])))
+
+(defn NYCmap-inner [countdata]
   (let [width 960
         height 500
         projection (.. js/d3.geo
@@ -84,196 +165,55 @@
                        (scale 50000)
                        (translate #js [(/ width 2) (/ height 2)]))
 
-        formatNumber (fn [d]  ((.format js/d3 ",.01f") d))
         path (.. js/d3.geo path (projection projection))
-        radius (.. js/d3.scale
-                   sqrt
-                   (domain #js [0 1e6])
-                   (range  #js [0 15] ))
+
+        getlatlon (fn [datamap] (let [latitude (:Latitude datamap)
+                                      longitude (:Longitude datamap)
+                                      [lon lat] (js->clj (projection #js [longitude latitude])) ]
+                                  (merge datamap {:d3lat lon  :d3lon lat})))
 
         update  (fn [comp]
-                  (let [{:keys [mapdata countdata]} (reagent/props comp)
-                        json (clj->js mapdata)
-                        counts (clj->js countdata)
-                        svg  (.. js/d3 (select "svg"))]
-
-                    (println json)
-                    (println counts)
-
-                    ;; draw nyc map
-                    (.. svg (append "g")
-                        (attr "id" "boroughs")
-                        (selectAll ".state")
-                        (data (.-features json))
-                        enter
-                        (append "path")
-                        (attr "class" (fn [d] (str "borough " d.properties.borough)))
-                        (attr "d" path)
-                        (style "fill" (fn [d] d.colors)))
-
-                    ;; draw circles
+                  (let [countdata (-> comp reagent/props :data)]
+                    (println countdata)
                     (.. js/d3
-                        (select "svg")
-                        (append "g")
-                        (attr "id" "circle")
                         (selectAll "circle")
-                        (data counts)
-                        enter
-                        (append "scg:circle")
-                        (attr "cx" (fn [d] (first (.-location d))))
-                        (attr "cy" (fn [d] (second (.-location d))))
-                        (attr "cr" 5)
-                        (attr "class" "bubble")
-                        )
-                    ))]
+                        (data (clj->js countdata))
+                        (attr "r"  (fn [d] (.-size d))))
+
+                    (println "In the update function")))]
 
     (reagent/create-class
       {:reagent-render (fn []
                          [:div
                           [:h2 "Awesome NYC Map"]
-                          [:div#map [:svg {:style {:height (str  height "px") :width (str width "px")}}]]])
+                          [:div#map
+                           [molecule-select-box]
+                           [:svg {:style {:height (str  height "px") :width (str width "px")}}]]])
 
-       :component-did-mount (fn [comp] (update comp))
+       :component-did-mount (fn [comp]
+                              (let [
+                                    ;countdata (-> comp reagent/props :data)
+                                    countdata-updated (map getlatlon countdata)
+                                    svg  (.. js/d3 (select "svg"))]
+                                (println countdata)
+                                (do (load-map "js/nyc.json" svg path))
+                                    (draw-points svg (clj->js countdata-updated))))
+
        :component-did-update update
        :display-name "d3map-inner"})))
 
 
 
-;(defn d3map-inner []
-;  (let [width 960
-;        height 600
-;        formatNumber (fn [d]  ((.format js/d3 ",.01f") d))
-;        path (.. js/d3.geo path (projection nil))
-;        radius (.. js/d3.scale
-;                   sqrt
-;                   (domain #js [0 1e6])
-;                   (range  #js [0 15] ))
-;        update  (fn [comp]
-;                  (let [mapclj (reagent/props comp)
-;                        json (clj->js mapclj)
-;                        svg  (.. js/d3 (select "svg"))]
-;
-;                    (.. svg (append "g")
-;                        (attr "class" "legend")
-;                        (attr "transform" (str "translate(" (- width 50) "," (- height 20) ")"))
-;                        (selectAll "g")
-;                        (data #js [1e6 5e6 1e7])
-;                        enter
-;                        (append "g"))
-;
-;                    (.. svg
-;                        (append "path")
-;                        (datum (.feature js/topojson json (.. json -objects -nation)))
-;                        (attr "class" "land")
-;                        (attr "d" path))
-;
-;                    (.. svg
-;                        (append "g")
-;                        (attr "class" "bubble")
-;                        (selectAll "circle")
-;                        (data (.. js/topojson
-;                                  (feature json (.. json -objects -counties))
-;                                  -features))
-;                        (sort (fn [a b] (-  (.. b -properties -population) (.. a -properties -population))))
-;                        enter
-;                        (append "circle")
-;                        (attr "transform" #(str "translate(" (.centroid path %) ")"))
-;                        (attr "r" (fn [d] (radius (.. d -properties -population))))
-;                        (append "title")
-;                        (text (fn [d] (str (.. d -properties -name) " Population " (formatNumber (.. d -properties -population))))))
-;                    ))]
-;
-;    (reagent/create-class
-;      {:reagent-render (fn []
-;                         [:div
-;                          [:h2 "Awesome Map"]
-;                          [:div#map [:svg {:style {:height (str  height "px") :width (str width "px")}}] ]])
-;
-;       :component-did-mount (fn [comp] (update comp))
-;       :component-did-update update
-;       :display-name "d3map-inner"})))
-
-
-;(defn d3-bargraph-inner []
-;  (let [width 960
-;        height 200
-;        formatNumber (fn [d]  ((.format js/d3 ",.01f") d))
-;        path (.. js/d3.geo path (projection nil))
-;        radius (.. js/d3.scale
-;                   sqrt
-;                   (domain #js [0 1e6])
-;                   (range  #js [0 15] ))
-;        update  (fn [comp]
-;                  (let [mapclj (reagent/props comp)
-;                        json (clj->js mapclj)
-;                        svg  (.. js/d3 (select "svg"))]
-;
-;                    (.. svg (append "g")
-;                        (attr "class" "legend")
-;                        (attr "transform" (str "translate(" (- width 50) "," (- height 20) ")"))
-;                        (selectAll "g")
-;                        (data #js [1e6 5e6 1e7])
-;                        enter
-;                        (append "g"))
-;
-;                    (.. svg
-;                        (append "path")
-;                        (datum (.feature js/topojson json (.. json -objects -nation)))
-;                        (attr "class" "land")
-;                        (attr "d" path))
-;
-;                    (.. svg
-;                        (append "g")
-;                        (attr "class" "bubble")
-;                        (selectAll "circle")
-;                        (data (.. js/topojson
-;                                  (feature json (.. json -objects -counties))
-;                                  -features))
-;                        (sort (fn [a b] (-  (.. b -properties -population) (.. a -properties -population))))
-;                        enter
-;                        (append "circle")
-;                        (attr "transform" #(str "translate(" (.centroid path %) ")"))
-;                        (attr "r" (fn [d] (radius (.. d -properties -population))))
-;                        (append "title")
-;                        (text (fn [d] (str (.. d -properties -name) " Population " (formatNumber (.. d -properties -population))))))
-;                    ))]
-;
-;    (reagent/create-class
-;      {:reagent-render (fn []
-;                         [:div
-;                          [:h2 "Awesome Map"]
-;                          [:div#map [:svg {:style {:height (str  height "px") :width (str width "px")}}] ]])
-;
-;       :component-did-mount (fn [comp] (update comp))
-;       :component-did-update update
-;       :display-name "d3map-inner"})))
-
 (defn NYCmap-outer []
-  (let [mapdata (re-frame/subscribe [:mapdata])
-        countdata (re-frame/subscribe [:countdata])]   ;; obtain the data
+  (let [countdata (re-frame/subscribe [:countdata])
+        activemol (re-frame/subscribe [:activemolecule])]
+    (println @countdata)
     (fn []
-      [NYCmap-inner {:countdata @countdata :mapdata @mapdata}])))
-
-
-;(defn simplecomponent []
-;  (let [mapdata (re-frame/subscribe [:mapdata])]
-;    (println @mapdata)
-;    [:p (str (first @mapdata))]))
-
-;(defn d3map-outer []
-;  (let [mapdata (re-frame/subscribe [:mapdata])]   ;; obtain the data
-;    (fn []
-;      [d3map-inner (first @mapdata)])))
-
-;(defn d3-bargraph-outer []
-;  (let [mapdata (re-frame/subscribe [:mapdata])]   ;; obtain the data
-;    (fn []
-;      [d3map-inner (first @mapdata)])))
+      [:div
+       [NYCmap-inner @countdata]
+       (when @activemol [:h3 (str "Activemol: " @activemol)])])))
 
 (let []
-  (dispatch-sync [:initialize-mapdata])
-  (dispatch-sync [:initialize-countdata])
-  (reagent/render [NYCmap-outer] (. js/document (getElementById "app")))
-  )
-
-
+  (dispatch-sync [:initialize-db])
+  ;(dispatch-sync [:initialize-countdata])
+  (reagent/render-component [NYCmap-outer] (. js/document (getElementById "app"))))
